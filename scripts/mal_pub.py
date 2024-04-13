efa2fa363693c5e208f7baae11e5d6419789c87a
Erick Silva <ericksilvarod@gmail.com>
2024-04-13 13:14:31 +0300

    searching for PUBREC

--- scripts/mal_pub.py
+++ scripts/mal_pub.py
@@ -11,7 +11,7 @@ import random
 
 broker_ip = "192.168.122.48"
 broker_port = 1883
-number_packets = 20000
+number_packets = 1
 
 # MQTT Connect Packet for version 3.1.1
 def create_connect_packet(client_id="cm"):
