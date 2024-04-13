104cb9da15662cbb4bb84cff11ff9c3af31eb5af
Erick Silva <ericksilvarod@gmail.com>
2024-04-13 18:41:04 +0300

    trying to fix ACK

--- scripts/mal_pub.py
+++ scripts/mal_pub.py
@@ -103,7 +103,6 @@ ack = establish_tcp_connection(ip, src_port, broker_port)
 # Craft an MQTT CONNECT packet
 connect_pkt = create_connect_packet()
 send(ip/TCP(sport=src_port, dport=broker_port, flags="PA", seq=ack.seq, ack=ack.ack)/connect_pkt)
-seq = ack.seq + len(connect_pkt) 
 
 # Wait a bit for the broker to process our connection
 #time.sleep(2)
@@ -116,11 +115,11 @@ for _ in range(number_packets):
 
     publish_pkt = create_publish_packet(topic, message)
     # publish_pkt.show()
-
+    seq = ack.seq + len(connect_pkt) 
     message_id = random.randint(0, 0xFFFF)  # Generate a unique message ID for each message
     publish_pkt = MQTT(QOS=2)/MQTTPublish(topic=topic, value=message, msgid=message_id)
     send(ip/TCP(sport=src_port, dport=broker_port, flags="PA", seq=seq, ack=ack.ack)/publish_pkt)
-    seq += len(publish_pkt) + 1 # +1 for the ACK
+    seq += len(publish_pkt) # +1 for the ACK?
     #time.sleep(5)
 
 
