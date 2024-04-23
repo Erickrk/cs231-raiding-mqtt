b6dee1bd791c87f6898802899e9b70db220649f0
Erick Silva <ericksilvarod@gmail.com>
2024-04-23 19:16:05 +0300

    testing to see if message size impacts sync

--- scripts/publisher.py
+++ scripts/publisher.py
@@ -13,10 +13,12 @@ client.connect(broker_address)
 
 # Publish sensor data with QoS 2 and retain flag
 counter =10
+MAX_SIZE = 60 * 1024  # 60 KB
+message = "A" * MAX_SIZE
 
 for _ in range(counter):
     sensor_data = random.randint(0, 999)
-    client.publish(topic, sensor_data, qos=2, retain=True)
+    client.publish(topic, message, qos=2, retain=True)
     print(f"Message {counter} published to {topic}: {sensor_data}")
     time.sleep(0.01)  # Wait for 5 seconds before next publish
 
