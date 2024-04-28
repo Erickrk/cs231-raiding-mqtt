6757fecbf5408313073ec788a49d62d69be0c806
Erick Silva <ericksilvarod@gmail.com>
2024-04-28 17:25:26 +0300

    making both pub_paho more alike

--- scripts/pub_paho_v1.py
+++ scripts/pub_paho_v1.py
@@ -12,13 +12,13 @@ client = mqtt.Client("P1")
 client.connect(broker_address) 
 
 # Publish sensor data with QoS 2 and retain flag
-counter =10
+counter = 100
 MAX_SIZE = 60 * 1024  # 60 KB
 message = "A" * MAX_SIZE # This generates an interesting behavior in the broker, exchanging a lot of ACKs
 
-for _ in range(counter):
+for i in range(counter):
     # sensor_data = random.randint(0, 999)
     client.publish(topic, message, qos=2, retain=True)
-    #print(f"Message {counter} published to {topic}: {sensor_data}")
+    print(f"Message {i} published to {topic}")
     time.sleep(0.01)  # Wait for 5 seconds before next publish
 
--- scripts/pub_paho_v2.py
+++ scripts/pub_paho_v2.py
@@ -4,7 +4,6 @@ import random
 import paho.mqtt.client as mqtt
 
 # Based on: https://eclipse.dev/paho/files/paho.mqtt.python/html/migrations.html
-
 # This should be static
 broker_address = "192.168.122.48" 
 topic = "sensor/data"
@@ -13,17 +12,14 @@ topic = "sensor/data"
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, client_id="P1")
 client.connect(broker_address) 
 
-
 # Publish sensor data with QoS 2 and retain flag
-MAX_SIZE = 240 * 1024  # 60 KB
+counter = 100
+MAX_SIZE = 60 * 1024  # 60 KB
 message = "A" * MAX_SIZE # This generates an interesting behavior in the broker, exchanging a lot of ACKs
 
-counter = 100000
-i=0
-while i < counter:
+for i in range(counter):
     # sensor_data = random.randint(0, 999)
     client.publish(topic, message, qos=2, retain=True)
     print(f"Message {i} published to {topic}")
-    i += 1
     time.sleep(0.01)  # Wait before next publish, increasing this didnt made it work
 
