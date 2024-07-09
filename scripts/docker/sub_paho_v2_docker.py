7d6867e955b131c475c85f6b329d985a27e823a5
Erickrk <ericksilvarod@gmail.com>
2024-07-09 18:20:40 +0300

    added on connect to see reason code after connect

--- scripts/docker/sub_paho_v2_docker.py
+++ scripts/docker/sub_paho_v2_docker.py
@@ -29,7 +29,7 @@ def on_message(client, userdata, msg):
 broker_address = "localhost"
 
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, client_id="S1")  # create new instance
-#client.on_connect = on_connect(client=None, userdata=None, flags=None, reason_code=None, properties=None)
+client.on_connect = on_connect(client=None, userdata=None, flags=None, reason_code=None, properties=None)
 client.on_message = on_message
 
 client.connect(broker_address)  # connect to broker with QoS 2
