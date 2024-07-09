a30eefdc17df9fca58038bd4f5782da964c17ed1
Erickrk <ericksilvarod@gmail.com>
2024-07-09 18:25:42 +0300

    added v1 and pointed to localhost

---
+++ scripts/docker/pub_paho_v1_docker.py
@@ -0,0 +1,31 @@
+'''
+    File: pub_paho_v1.py
+    Author: Erick Silva
+    Course: CS231
+    This script is part of the coursework for CS231. It is used to publish sensor data
+    to a specific MQTT topic using the Paho MQTT client version 1.3.
+'''
+
+import time
+
+import paho.mqtt.client as mqtt
+
+# This should be static
+broker_address = "localhost" 
+topic = "sensor/data" 
+
+# create new client instance
+client = mqtt.Client("P1")  
+client.connect(broker_address) 
+
+# Publish sensor data with QoS 2 and retain flag
+counter = 100
+MAX_SIZE = 60 * 1024  # 60 KB
+message = "A" * MAX_SIZE # This generates an interesting behavior in the broker, exchanging a lot of ACKs
+
+for i in range(counter):
+    # sensor_data = random.randint(0, 999)
+    client.publish(topic, message, qos=1, retain=True)
+    print(f"Message {i} published to {topic}")
+    time.sleep(0.01)  # Wait for 5 seconds before next publish
+
