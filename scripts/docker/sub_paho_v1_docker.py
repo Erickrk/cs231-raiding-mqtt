fa4c0e1ac52476316d0f855cb0598229704cc6db
Erickrk <ericksilvarod@gmail.com>
2024-07-13 21:42:39 +0300

    updated v1 codes to work with localhost

--- scripts/docker/sub_paho_v1_docker.py
+++ scripts/docker/sub_paho_v1_docker.py
@@ -17,7 +17,7 @@ def on_message(client, userdata, msg):
     print(f"Received message: {msg.payload.decode()} on topic {msg.topic}")
 
 # Configure the MQTT client
-broker_address = "192.168.122.48" 
+broker_address = "localhost" 
 client = mqtt.Client("C1")  # create new instance
 client.on_connect = on_connect
 client.on_message = on_message
